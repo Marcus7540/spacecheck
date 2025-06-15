6dd4fc7479d71bc6bb8c61780a3f5b89e1984bd5
Marcus <133431095+Marcus7540@users.noreply.github.com>
2025-06-15 22:40:22 +0200

    Fix

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
 # Copy binary
-sudo cp bin/Release/net*/linux-x64/publish/SpaceCheck /usr/local/bin/spacecheck
+sudo cp bin/Release/net*/linux-x64/publish/Dotnet /usr/local/bin/spacecheck
 sudo chmod +x /usr/local/bin/spacecheck
 
 # Add alias
